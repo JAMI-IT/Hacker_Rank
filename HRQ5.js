0e77ed5228e527c952c844a6740eb6aa59731d27
jami-it <abdulrehmanjami73@gmail.com>
2022-06-09 15:04:00 +0500

    HR

---
+++ HRQ5.js
@@ -0,0 +1,14 @@
+function staircase(n) {
+    // Write your code here
+    let c="#";
+    let b=" ";
+    for (let i = n; i > 0; i--) {
+
+        
+
+    b=b.concat(c);
+   console.log(b);
+}
+}
+let n=6;
+staircase(n);
