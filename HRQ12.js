0017fbbce5149a1b771ba63c9700c1501a3587b3
jami <abdulrehmanjami73@gmail.com>
2022-06-16 20:40:49 +0500

    HR

--- HRQ12.js
+++ HRQ12.js
@@ -1,36 +1,25 @@
 function breakingRecords(scores) {
     // Write your code here
-    let x=-Infinity;
-    let y=Infinity;
-    let m=0-1;
-    let min=0-1; 
-    for (let i = 0; i < scores.length; i++) {
-        if (scores[i]>x) {
-            x=scores[i];
-            m=m+1;
-        }
-        else {
-            if (scores[i]=x) {
-                m=m;
-            }    
-        }
-    }
-    for (let z = 0; z < scores.length; z++) {
-        if (scores[z]<y) {
-            y=scores[z];
-            console.log(y);
-            min=min+1;
-        }
-        else
-        {
-        if (scores[z]=x) {
-            min=min;
-        }
-        }
-    }
-return [m,min];
+    var m=scores[0];
+    var min=scores[0];
+    var maxi=0;
+    var mini=0;
+for (let i = 0; i < scores.length; i++) {
+  
+     if(scores[i]>m){
+        m=scores[i];
+        maxi=maxi+1;
+    }  
+    else if(scores[i]<min){
+        min=scores[i];
+        mini=mini+1;
+    }  
+
+}
+return [maxi,mini]
+
 }
 
-// let a=[3,4,21,36,10,28,35,5,24,42];
-let b=[10, 5 ,20 ,20 ,4, 5 ,2 ,25,1]
-console.log(breakingRecords(b));
+let scores=[3 ,4 ,21 ,36 ,10 ,28 ,35 ,5 ,24 ,42]
+
+console.log(breakingRecords(scores));
