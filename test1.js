0689654ca4fb4baab755c3f0ff6237263fd4b13f
jami <abdulrehmanjami73@gmail.com>
2022-06-20 10:32:45 +0500

    HR

--- test1.js
+++ test1.js
@@ -1,10 +1,19 @@
 function bonAppetit(bill, k, b) {
     // Write your code here
 
- 
+var total = bill.reduce((a,b) => a+b);
+total=total-bill[k];
+
+if (total/2==b) {
+    console.log("Bon Appetit");
+}
+else{
+    console.log(b-total/2);
 }
 
-let bill=[3,10,2,9,1];
+}
+
+let bill=[3,10,2,9];
 let k=1;
 let b=7;
 bonAppetit(bill,k,b);
